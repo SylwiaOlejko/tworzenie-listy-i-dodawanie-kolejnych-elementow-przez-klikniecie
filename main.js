7c69e00783696099e3389bd13ebfc322642b0805
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-10-24 22:44:32 +0200

    create button

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
- // tutaj kod
+ const btn = document.createElement('button');
+ btn.textContent = "Dodaj 10 elementów listy";
+ btn.style.fontSize = "28px";
+
+ document.body.appendChild(btn);
+ const ul = document.createElement('ul');
+ document.body.appendChild(ul);
+ btn.addEventListener('click', createLiElements);
 }
 
 const createLiElements = () => {
- // tutaj kod
+    console.log("klik");
 }
 
 init()
 
-test
